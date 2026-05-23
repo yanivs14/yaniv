613f231128977e0710f52c67d9e892396c8fb5ef
Yaniv Shabtiel <yanivs14@gmail.com>
2026-05-23 02:02:59 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,12 +20,6 @@ const Footer = lazy(() => import("../components/landing/Footer"));
 export default function Home() {
   const { loading } = useSiteContent();
 
-  if (loading) return (
-    <div className="min-h-screen bg-dark-bg flex items-center justify-center">
-      <div className="w-8 h-8 border-4 border-dark-border border-t-orange-red rounded-full animate-spin" />
-    </div>
-  );
-
   return (
     <div className="bg-dark-bg min-h-screen">
       <Navbar />
